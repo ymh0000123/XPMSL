0871c22a1471d4adea44616bae296d2def354b92
没用的小废鼠 <107793048+ymh0000123@users.noreply.github.com>
2024-02-11 14:16:50 +0800

    vue

---
+++ vuepress/docs/.vuepress/config.js
@@ -0,0 +1,49 @@
+import { defaultTheme } from 'vuepress'
+export default {
+    base: '/',
+    title: 'XPMSL',
+    description: 'Just playing around',
+    head: [
+        ['link', { rel: 'icon', href: '/ico.png' }]
+    ],
+    plugins: [
+    ],
+    theme: defaultTheme({
+        Locale: 'auto',
+        colorModeSwitch: "true",
+        home: "/",
+        logo: '/ico.png',
+        repo: 'ymh0000123/XPMSL',
+        sidebar: [
+            // SidebarItem
+            {
+                text: 'XPMSL',
+                link: '/',
+                children: [
+                    {
+                        text: 'Github',
+                        link: 'https://github.com/ymh0000123/XPMSL',
+                        children: [],
+                    },
+                    '/zh-cn/',
+                    '/zh-cn/historicalversion',
+                ],
+            },
+        ],
+        navbar: [
+            {
+                text: 'Home',
+                link: '/'
+            },
+            {
+                text: "其他",
+                children: [
+                    {
+                        text: '历史版本',
+                        link: "/zh-cn/historicalversion"
+                    }
+                ],
+            },
+        ]
+    }),
+}
---
+++ vuepress/docs/README.md
@@ -0,0 +1,18 @@
+---
+home: true
+heroImage: /ico.png
+heroText: XPMSL
+tagline: XPMSL官方文档
+actions:
+  - text: 快速上手
+    link: /zh-cn/
+    type: primary
+features:
+- title: 简单快速
+  details: 使用python编写，并且有gui操作界面。
+- title: 免费开源
+  details: 代码需要遵守GPL-3.0 license。
+- title: 上手简单
+  details: 下载完成直接运行就可以使用
+footer: GPL-3.0 license | XPMSL免费开源 | Docs Powered by VuePress
+---
