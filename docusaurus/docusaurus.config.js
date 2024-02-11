5ac573fa006d3114daf98339a85c2777e5e41c34
没用的小废鼠 <107793048+ymh0000123@users.noreply.github.com>
2024-02-11 13:25:57 +0800

    1

---
+++ docusaurus/docusaurus.config.js
@@ -0,0 +1,110 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import {themes as prismThemes} from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'XPMSL',
+  tagline: 'XPMSL官方文档',
+  favicon: 'https://xiaofeishu-picture.pages.dev/picture/5.png',
+
+  // Set the production url of your site here
+  url: 'https://xpmsl.pages.dev/',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/XPMSL/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'cn',
+    locales: ['cn'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/ymh0000123/XPMSL/tree/main/docs/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/ymh0000123/XPMSL/tree/main/docs/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'https://xiaofeishu-picture.pages.dev/picture/5.png',
+      navbar: {
+        title: 'XPMSL官方文档',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'https://xiaofeishu-picture.pages.dev/picture/5.png',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: '快速开始',
+          },
+          {
+            href: 'https://github.com/ymh0000123/XPMSL',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'GitHub',
+                href: 'https://github.com/ymh0000123/XPMSL',
+              },
+            ],
+          },
+        ],
+        copyright: ` ${new Date().getFullYear()} | GPL-3.0 license | XPMSL免费开源`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
